b203e6c07f4597f1d391382e3fd1fe816b50a253
Andreas Gottwald <andreas.gottwald@hotmail.de>
2023-12-27 12:41:26 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Hello World</h2>
+      <label htmlFor="input"> Input</label>
+      <input id="input"></input>
+      <a className="article__link" href="https://www.wikipedia.org/">
+        Link to wikipedia
+      </a>
+    </article>
+  );
 }
