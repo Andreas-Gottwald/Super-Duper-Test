9c965721baf39ec9dabb5ee5d30fd56adf83cda8
Andreas Gottwald <andreas.gottwald@hotmail.de>
2024-01-04 19:10:24 +0100

    hue

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
+import { getDrinks } from "./components/drinks";
+import { useEffect } from "react";
 
 export default function App() {
-  return <Article />;
-}
+  const [count, setCount] = useState(0);
 
-function Article() {
+  useEffect(() => {
+    console.log("the count is:" + count);
+  }, [count]);
+
+  const handleWaterButtonClick = () => {
+    // Handle logic for water button click
+    // For now, let's just increment the count
+    setCount(count + 1);
+  };
+
   return (
-    <article className="article">
-      <h2 className="article__title">Hello World</h2>
-      <label htmlFor="input"> Input</label>
-      <input id="input"></input>
-      <a className="article__link" href="https://www.wikipedia.org/">
-        Link to wikipedia
-      </a>
-    </article>
+    <>
+      <h1>Count: {count}</h1>
+      <button className="redButton" onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+      <button className="blueButton" onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+      <ul>
+        {getDrinks().map(({ id, name, color }) =>
+          name === "water" ? (
+            <button key={id} style={{ color }} onClick={handleWaterButtonClick}>
+              {name}
+            </button>
+          ) : (
+            <li key={id} style={{ color }}>
+              {name}
+            </li>
+          )
+        )}
+      </ul>
+    </>
   );
 }
---
+++ src/components/drinks.js
@@ -0,0 +1,11 @@
+// drinks.js
+const drinks = [
+  { id: 0, name: "water", color: "yellow", backgroundColor: "green" },
+  { id: 1, name: "lemonade" },
+  { id: 2, name: "coffee" },
+  { id: 3, name: "tea" },
+];
+
+export function getDrinks() {
+  return drinks;
+}
